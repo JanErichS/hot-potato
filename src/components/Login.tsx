6bdf18c679b8ccf64347f6fbb29058d4732902ba
Jan-Erich <146342622+JanErichS@users.noreply.github.com>
2024-09-16 17:31:50 +0300

    feat: 24-09-16 lesson done

---
+++ src/components/Login.tsx
@@ -0,0 +1,67 @@
+import { FormEvent, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+export default function Login() {
+  //   const [username, setUsername] = useState("");
+  //   const [password, setPassword] = useState("");
+
+  const [form, setForm] = useState({ username: "", password: "" });
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      [event.target.name]: event.target.value
+    }));
+  };
+
+  //   useEffect(() => {
+  //     console.log("Yippie!");
+  //   }); // Alati
+  //   useEffect(() => {
+  //     console.log("Yippie 2!");
+  //   }, []); // Üks kord.
+  //   useEffect(() => {
+  //     console.log("Yippie 2!");
+  //   }, [username]); // Siis kui username muutub.
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Stack spacing={5}>
+        <Typography variant="h1" color="initial">
+          Login
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="outlined-basic"
+            name="username"
+            label="Username"
+            variant="outlined"
+            onChange={handleChange}
+            //   onChange={(event) => setUsername(event.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            name="password"
+            label="Password"
+            variant="outlined"
+            onChange={handleChange}
+            //   onChange={(event) => setPassword(event.target.value)}
+          />{" "}
+          <Button variant="outlined" color="primary" type="submit">
+            Login
+          </Button>
+        </form>
+      </Stack>
+    </Box>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import Login from "./components/Login";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/extra" element={<h1>Extra</h1>} />
+      <Route path="/contact" element={<h1>Contact</h1>} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
